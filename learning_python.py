e9abdd7f549f54dfa61792544c62513c6103e55d
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2021-02-27 13:34:25 -0800

    Update learning_python.py

--- learning_python.py
+++ learning_python.py
@@ -3273,7 +3273,7 @@ print(len(b))
 del b
 '''
 
-
+'''
 def round_sum(a, b, c):
   return round10(a) + round10(b) + round10(c)
 
@@ -3286,31 +3286,223 @@ def round10(num):
   return num
 
 print(round_sum(16, 17, 18))
+'''
+
+
+def q1():
+	print("hello" * 5)
+
+def q2():
+	x, y = 4, 5
+	y, x = x, y
+	print(x)
+	print(y)
+
+def q3():
+	z = 9
+	lst = [0] * z
+	print(lst[:-1])
+
+def q4():
+	def help(x):
+		return x % 2 == 0
+
+	lst = [i**2 for i in range(10)] 
+	lst = filter(help, lst) 
+	print(list(lst)[::2]) 
+
+def q5():
+	z = 0
+	for i in range(1, 10):
+		if (i + 1 // 2) % 7 == 0:
+			break
+		else:
+			z += int(i % 2 == 0)
+			print(z)
+	else:
+		print('end')
+
+def q6():
+	import math
+
+	for i in range(1, 100):
+		if math.sqrt(i) == (i - 5)**2:
+			print(i)
+			break
+	else:
+		print('no')
+
+def q7():
+	class A:
+		def __init__(self, x):
+			self.x = x
+
+		def __eq__(self, o):
+			return self.x == o.x
+
+	a = A(2)
+	b = A(2)
+	print(a == b)
+	print(a is b)
+	print(b is a)
+	print(a is a)
+	print(a == a)
+
+def q8():
+	class A:
+		def __init__(self, x, y):
+			self.x = x
+			self.y = y
+
+		def print(self):
+			print(self.x, self.y)
+
+	class B(A):
+		def print(self):
+			print(self.y, self.x)
+
+	class C(B):
+		def __init__(self, x, y, z):
+			super().__init__(x, y)
+			self.z = z
+
+	d = A(2, 4)
+	e = B(4, 5)
+	g = C(3, 4, 7)
+	g.x = g.z
+	d.print()
+	e.print()
+	g.print()
+
+def q9():
+	def x(z):
+		def q(x, y):
+			x = y + z + x
+			print(x)
+
+		return q
+    
+	for i in range(10):
+		func = x(i)
+		func(i, i-1)
+    
+def q10():
+	def d(f):
+		def w(*args, **kwargs):
+			r = f(*args, **kwargs) 
+			r += 1 
+			return r 
+
+		return w
+
+	@d
+	def a(x):
+		return x + 1
+
+	print(a(5))
+
+def q11():
+	print(*list(map(lambda x: chr(ord(x) + 1), ["a", "b", "c"])))
+
+def q12():
+	x = 0.1
+	y = 0.10000000000000001
+	print(x == y)
+
+def q13():
+	x = [True, 1, "a", "b", "2"]
+	print(any(x)) 
+
+def q14():
+	x = ["a", 1, 2, 3, 4]
+	y = z = x
+	z[1] = 7
+	y[3] = 2
+	x[2] = 9
+	print(x)
+	print(y)
+	print(z)
+
+def q15():
+	print(1 == True)
+	print("1" == 1)
+
+def q16():
+	x = b'1001'
+	y = b'1010'
+	z = x + y
+	print(z)
+
+def q17():
+	x = 0b1001
+	y = 0b1010
+	z = x + y
+	print(z)
+#1,2,3, half 4, half 5, 6, 8 half, 10 half, 11 half, 13, 15, 16 half
+#7? #a is b false?
+#9 how is fuin q being ran?
+#8 half ish 1 line off
+#11 doesnt print as a list but as a long string?
+#12 on the 17th decimal place the values 0.1 and 0.100000000000000001 are equal
+#14 all referencing the same memory location? So one change changes all of them??
+#Because of immutable vs mutable?, list is mutable
+#16 includes b and string but adds the numbers as string (binary???)
+#Counting binary get it now
+
+def count_binary():
+    print()
+    print(9)
+    print(10)
+    print(19)
+    print()
+
+    print(0b0)   #0
+    print(0b1)   #1
+    print(0b10)  #2
+    print(0b11)  #3
+    print(0b100) #4  
+    print(0b101) #5  2 after 1 == 2
+    print(0b110) #6  X2 number of 0s 100000 has 5 so 2 to power of 5 = 64
+    print(0b111) #7
+    print(0b1000) #8
+    print(0b1001) #9
+    print(0b1010) #10
+    print(0b1011) #11
+    print(0b1100) #12
+    print(0b1101) #13
+    print(0b1110) #14
+    print(0b1111) #15
+    print(0b10000) #16
+    print(0b100000) #32
+    print(0b1000000)
 
 
 
 
 
+    '''
+    0                   0
+    1                   1
+    10                  2
+    11                  3
+    100                 4
+    101                 5
+    110                 6
+    111                 7
+    1000                8
+    1001                9
+    1010                10
+    1011                11
+    1100                12
+    1101                13
+    1110                14
+    1111                15
 
+    '''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #print(0b101)
+    #print(0b111)
+    #ANSWERS REVIEW....
 
 
 
