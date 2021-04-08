2776d7ed7548ddc3e99a8f9b7431ef2073762f72
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2021-04-07 20:55:46 -0700

    password generating and basic decryption

--- password_generator.cpp
+++ password_generator.cpp
@@ -3,49 +3,86 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
-string randomGenerator();
+string randomGenerator(int numberOfCharacters);
 char randomCapitalCharacter();
 char randomLowerCharacter();
 char randomSpecialCharacter();
-int randomInt();
+char randomInt();
+
+string decryption(string crackThisPassword, int &t);
 
 /* TODO:
+    GET RANDOM TO ACTUALLY BE RANDOM
+    DEFAULT VALUE FOR FUNCTIONS? NOT WORKING
     true and false for if each function allowed to be called in the random.
-    random length from 8-12
     each function must be called atleast one time.
     random order of using each function
+
+
+    decryption
+    DO REAL TIME FOR DECRYPTION
+    DO for loops num, alphabetical, etc.. 1a,1b,1c .... 2a,2b,2c
+    RANDOMIZE ORDER FOR PASSWORD AS WELL AS FOR DECRYPTION
     */
 
 int main(){
     srand((unsigned int) time(NULL));
-    cout << randomGenerator() << endl;
+    string password = randomGenerator(6);
+    int time = 0;
+    cout << password << endl << decryption(password, time) << " decrypted in "<< time << " attempts." << endl << endl;
     return 0;
 }
-int randomInt(){
+
+char randomInt(){
     //TODO: Make actually random number
-    int random_number = std::rand()% 10;
-    return random_number;
-}
-char randomCapitalCharacter(){
-    return 65 + rand()%26; //65 = A
+    std::string test = to_string(std::rand()% 10);
+    char randomNumberChar = test.at(0);
+    return randomNumberChar;
 }
 char randomLowerCharacter(){
     return 97 + rand()%26; //97 = a
+}
+char randomCapitalCharacter(){
+    return 65 + rand()%26; //65 = A
 }
 char randomSpecialCharacter(){
     /** Special characters are split by numbers so we pick a random from one of the 2 groups.*/
     return (rand()%2) ? char(33 + rand()%14) : char(58 + rand()%8); 
 }
-string randomGenerator(){
-    std::string word = "This will turn into a random generator!";
-    cout << randomInt() << endl;
-    cout << randomCapitalCharacter() << endl;
-    cout << randomLowerCharacter() << endl;
-    cout << randomSpecialCharacter() << endl;
-    return word;
+
+string randomGenerator(int numberOfCharacters = 8){
+    char randomCharacterTypes[4] = {randomLowerCharacter(),randomInt(), randomCapitalCharacter(), randomSpecialCharacter()};
+    std::string password;
+
+    for (int i = 0; i < numberOfCharacters; i++){
+        //password += randomCharacterTypes[(rand()%4)%4];
+        //above will work once i get random being actually random.
+        password += randomCharacterTypes[i%4];
+    }
+    return password;
+}
+
+string decryption(string crackThisPassword, int &t){
+    string cracked;
+    
+    while (cracked != crackThisPassword){
+        cracked;
+        cracked = randomLowerCharacter();
+        cracked += randomInt();
+        cracked += randomCapitalCharacter();
+        cracked += randomSpecialCharacter();
+        cracked += randomLowerCharacter();
+        cracked += randomInt();
+        cout << cracked << ' ';
+        t++;
+    }
+    
+    cout << endl << endl;
+    return cracked;
 }
 
 
