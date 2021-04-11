61abd8b579ef5e3b242733cdb051f05e6e10c18f
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2021-04-11 01:35:24 -0700

    brute force decryption up to password length of 10

--- password_generator.cpp
+++ password_generator.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <chrono>
 #include <tuple>
+#include <math.h>
+#include <functional>
 
 using namespace std::chrono;
 using namespace std;
@@ -18,7 +20,7 @@ char randomLowerCharacter();
 char randomSpecialCharacter();
 char randomInt();
 
-string decryption(string crackThisPassword, int &t);
+string decryption(string crackThisPassword, int &t, int passLength);
 string lowerCaseDecryption(string crackThisPassword, int &t, int length);
 string timeElapsed(long long seconds);
 
@@ -42,18 +44,17 @@ string timeElapsed(long long seconds);
 
 int main(){
     srand((unsigned int) time(NULL));
-    //string password = randomGenerator(2);
-    int passwordLength = 1;
-    string password = randomLowerGenerator(passwordLength);
+    string password = "t!Z";
+    int passwordLength = password.length();
+    //int passwordLength = 6;
+    //string password = randomGenerator(passwordLength);
     int iterations = 0;
 
     auto start = high_resolution_clock::now();
-    //string decrpyt = decryption(password, iterations);
-    string decrpyt = lowerCaseDecryption(password, iterations, passwordLength);
+    string decrpyt = decryption(password, iterations, passwordLength);
     auto stop = high_resolution_clock::now();
 
     string timeTaken = timeElapsed(duration_cast<seconds>(stop - start).count());
-
     cout << endl << "Password: " << password << " : " << decrpyt << endl << "Time taken to decrpt " << timeTaken << endl << "Decryption attemps: " << iterations << endl;
 
     return 0;
@@ -92,7 +93,6 @@ string randomGenerator(int numberOfCharacters = 8){
     }
     return password;
 }
-
 string randomLowerGenerator(int numberOfCharacters = 8){
     char randomCharacterTypes[4] = {randomLowerCharacter(),randomInt(), randomCapitalCharacter(), randomSpecialCharacter()};
     std::string password;
@@ -105,17 +105,34 @@ string randomLowerGenerator(int numberOfCharacters = 8){
     }
     return password;
 }
-
 string lowerCaseDecryption(string crackThisPassword, int &t, int length){
     int i = 97;
+    int x = 0;
     int guessingLength = 1;
     int lengthIterator = 0;
     string cracked;
-    /*
-    for (int i = 0; i< length; i++){
-        cracked += 'a';
+
+    while (true){
+        int nextLetter = pow(123, guessingLength);
+        cracked[0] = i% nextLetter;
+        cracked[1] = i% nextLetter;
+
+        cout << cracked << ' ';
+        t++;
+        i++;
+        if (i%123 == 0){
+                    i += 96;
+                    guessingLength++;
+                }
+
+        if (cracked == crackThisPassword){
+            cout << endl;
+            break;
+        }
+        
     }
-    */
+    return cracked;
+
     while (true){
         //cout << cracked << ' ';
         // string length depending on x length
@@ -190,38 +207,27 @@ string lowerCaseDecryption(string crackThisPassword, int &t, int length){
     // store b loop through all
     */
 }
-
-string decryption(string crackThisPassword, int &t){
+string decryption(string crackThisPassword, int &t, int passLength){
     string cracked;
-    /*
-    for (int i1 = 33; i1 <= 126; i1++){
-        for (int i2 = 33; i2 <= 126; i2++){
-            for (int i3 = 33; i3 <= 126; i3++){
+
+    switch (passLength){
+        case 1:
+            for (int i1 = 33; i1 <= 126; i1++){
                 cout << cracked << ' ';
                 cracked.clear();
                 cracked += i1;
-                cracked += i2;
-                cracked += i3;
                 t++;
                 if (cracked == crackThisPassword){
                     cout << endl;
                     return cracked;
                 }
             }
-        }
-    }
-    return "NONE";
-    */
-    for (int i1 = 33; i1 <= 126; i1++){
-        for (int i2 = 33; i2 <= 126; i2++){
-            for (int i3 = 33; i3 <= 126; i3++){
-                for (int i4 = 33; i4 <= 126; i4++){
+        case 2:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
                     cout << cracked << ' ';
                     cracked.clear();
                     cracked += i1;
-                    cracked += i2;
-                    cracked += i3;
-                    cracked += i4;
                     t++;
                     if (cracked == crackThisPassword){
                         cout << endl;
@@ -229,11 +235,141 @@ string decryption(string crackThisPassword, int &t){
                     }
                 }
             }
-        }
+        case 3:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        cout << cracked << ' ';
+                        cracked.clear();
+                        cracked += i1;
+                        cracked += i2;
+                        cracked += i3;
+                        t++;
+                        if (cracked == crackThisPassword){
+                            cout << endl;
+                            return cracked;
+                        }
+                    }
+                }
+            }
+        case 4:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        for (int i4 = 33; i4 <= 126; i4++){
+                            cout << cracked << ' ';
+                            cracked.clear();
+                            cracked += i1;
+                            cracked += i2;
+                            cracked += i3;
+                            cracked += i4;
+                            t++;
+                            if (cracked == crackThisPassword){
+                                cout << endl;
+                                return cracked;
+                            }
+                        }
+                    }
+                }
+            }
+        case 5:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        for (int i4 = 33; i4 <= 126; i4++){
+                            for (int i5 = 33; i5 <= 126; i5++){
+                                cout << cracked << ' ';
+                                cracked.clear();
+                                cracked += i1;
+                                cracked += i2;
+                                cracked += i3;
+                                cracked += i4;
+                                cracked += i5;
+                                t++;
+                                if (cracked == crackThisPassword){
+                                    cout << endl;
+                                    return cracked;
+                                } 
+                            }
+                        }
+                    }
+                }
+            }
+        case 6:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        for (int i4 = 33; i4 <= 126; i4++){
+                            for (int i5 = 33; i5 <= 126; i5++){
+                                for (int i6 = 33; i6 <= 126; i6++){
+                                    cout << cracked << ' ';
+                                    cracked.clear();
+                                    cracked += i1; cracked += i2; cracked += i3;
+                                    cracked += i4; cracked += i5; cracked += i6;
+                                    t++;
+                                    if (cracked == crackThisPassword){
+                                        cout << endl;
+                                        return cracked;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        case 7:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        for (int i4 = 33; i4 <= 126; i4++){
+                            for (int i5 = 33; i5 <= 126; i5++){
+                                for (int i6 = 33; i6 <= 126; i6++){
+                                    for (int i7 = 33; i7 <= 126; i7++){
+                                        cout << cracked << ' ';
+                                        cracked.clear();
+                                        cracked += i1; cracked += i2; cracked += i3;
+                                        cracked += i4; cracked += i5; cracked += i6; cracked += i7;
+                                        t++;
+                                        if (cracked == crackThisPassword){
+                                            cout << endl;
+                                            return cracked;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        case 8:
+            for (int i1 = 33; i1 <= 126; i1++){
+                for (int i2 = 33; i2 <= 126; i2++){
+                    for (int i3 = 33; i3 <= 126; i3++){
+                        for (int i4 = 33; i4 <= 126; i4++){
+                            for (int i5 = 33; i5 <= 126; i5++){
+                                for (int i6 = 33; i6 <= 126; i6++){
+                                    for (int i7 = 33; i7 <= 126; i7++){
+                                        for (int i8 = 33; i8 <= 126; i8++){
+                                            cout << cracked << ' ';
+                                            cracked.clear();
+                                            cracked += i1; cracked += i2; cracked += i3; cracked += i4;
+                                            cracked += i5; cracked += i6; cracked += i7; cracked += i8;
+                                            t++;
+                                            if (cracked == crackThisPassword){
+                                                cout << endl;
+                                                return cracked;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
     }
-    return "NOTHING";
-    
-    //return "Nothing";
+
+
     /*
     while (cracked != crackThisPassword){
         cracked;
